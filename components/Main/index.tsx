bb5f37fed1a8cb3959b0e4ec0e31f022e4498998
Artyom <artem.petrunin@list.ru>
2021-05-25 23:19:14 +0300

    adaptation

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -6,10 +6,11 @@ export default function() {
   return (
     <>
       <div className={style.header}>
-        <div className="container">
+        <div className={style.container}>
           <div className={style.header__inner}>
             <div className={style.info}>
               <img className={style.logo} src="/main/logo.svg" alt="Логотип" />
+              <img className={style.burger} src="/main/burger.svg" alt="Бургер" />
               <div className={style.box}>
                 <img src="/main/box.svg" alt="Бокс" />
               </div>
@@ -19,6 +20,22 @@ export default function() {
                   <input className={style.search__input} placeholder="Быстрый поиск" />
                 </label>
               </div>
+              <button className={style.search__btn}>
+                <svg width="18" height="24" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="6.27148" cy="6.51562" r="5.5" stroke="black"/>
+                  <path d="M14.2715 12.5156L10.2715 9.51562" stroke="black"/>
+                </svg>
+              </button>
+              <button className={style.searchPlus__btn}>
+                <svg width="18" height="24" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="6.27148" cy="6.51562" r="5.5" stroke="white"/>
+                  <path d="M14.2715 12.5156L10.2715 9.51562" stroke="white"/>
+                </svg>
+                <svg width="13" height="10" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6.72803 0L6.72803 12" stroke="white"/>
+                  <path d="M12.728 6L0.728027 6" stroke="white"/>
+                </svg>
+              </button>
               <button className={style.login__btn}>
                 <img src="/main/person.svg" alt="Логин" />
               </button>
@@ -29,6 +46,7 @@ export default function() {
               </div>
             </div>
             <div className={style.menu}>
+              <img className={style.bottles} src="/main/bottles.svg" alt="" />
               {["Тихие вины", "Игристые вины", "Крепленые вины", "Крепкий алкоголь", "Минеральная вода", "Сигареты", "Слабоалкогольные", "Безалкогольные"].map((name, index) => (
                 <a className={classNames([style.item, index === 0 ? style.item__active : ''])} href={name}>{name}</a>
               ))}
@@ -37,14 +55,14 @@ export default function() {
         </div>
       </div>
       <div className={style.content}>
-        <div className="container">
+        <div className={style.container}>
           <div className={style.content__inner}>
             <div className={style.titleWithPercent}>
               <div className={style.title}>Промо акции и скидки</div>
-              <img src="/main/percent.svg" alt="" />
+              <img className={style.percent} src="/main/percent.svg" alt="" />
             </div>
             <div className={style.breadCrumb}>
-              <img className={style.item} src="/main/arrow-back.svg" alt="" />
+              <img className={classNames([style.item, style.arrow])} src="/main/arrow-back.svg" alt="" />
               <a className={style.item} href="#">Главная</a>
               <div className={style.item}>/</div>
               <a className={style.item} href="#">Промо-акции</a>
@@ -60,36 +78,43 @@ export default function() {
               {[1, 2, 3].map((item) => (
                 <div className={style.item}>
                   <img className={style.photo} src="https://media.istockphoto.com/photos/red-wine-picture-id510963734" alt="" />
-                  <div className={style.title}>три бутылки по цене двух</div>
+                  <div className={style.title}>для прекрасных дам на 8 марта</div>
                   <div className={style.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id fusce pharetra, tellus purus. Ultricies bibendum vitae, elit, mauris, magna odio. Sed iaculis at amet et posuere in egestas lacus. Leo lectus massa, tristique hac at venenatis tempus morbi. Dui augue vivamus orci vel, at nisi elementum. Massa vitae cursus felis, urna convallis. Morbi at auctor pellentesque netus. Sit phasellus at mus id ac convallis pellentesque facilisis. Pharetra magna proin mattis augue sapien. Eget phasellus pharetra aliquet et commodo rhoncus aliquam. Sed imperdiet faucibus proin quis lacus leo ornare posuere. Nec amet, cras enim sit ac diam odio semper malesuada. Est purus et egestas pellentesque sagittis. In accumsan faucibus dignissim at turpis mi. Et ultrices sociis maecenas pretium tincidunt posuere gravida mauris amet. Aliquam magna purus vel ac arcu id. Nulla pharetra magnis risus.</div>
                   <img className={style.percent} src="/main/percent-black.svg" alt="" />
                 </div>
               ))}
-              <div className={style.footer}>
-                <button className={style.showMore__btn}>Показать ещё</button>
-                <button
-                  className={style.contentUp__btn}
-                  onClick={() => window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                  })}
-                >
-                  <span>Наверх</span>
-                  <img src="/main/arrow-up.svg" alt="" />
-                </button>
-              </div>
             </div>
           </div>
         </div>
       </div>
-      {/*<div className="container">*/}
-      {/*  <div className={style.wrapper}>*/}
-      {/*    <div className={style.header}>Header</div>*/}
-      {/*    <div className={style.sidebar}>Sidebar</div>*/}
-      {/*    <div className={style.content}>Content</div>*/}
-      {/*    <div className={style.footer}>Footer</div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className={style.showMore}>
+        <div className={style.container}>
+          <div className={style.showMore__inner}>
+            <button className={style.showMore__btn}>Показать ещё</button>
+            <button
+              className={style.contentUp__btn}
+              onClick={() => window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+              })}
+            >
+              <span>Наверх</span>
+              <img src="/main/arrow-up.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={style.footer}>
+        <div className={style.list}>
+          <div className={style.item}>Условия использования</div>
+          <div className={style.item}>О компании</div>
+          <div className={style.item}>Статьи</div>
+          <div className={style.item}>Доставка и оплата</div>
+          <div className={style.item}>Карта сайта</div>
+          <div className={style.item}>Политика конфиденциальности</div>
+        </div>
+        <div className={style.copyright}>Подробнее о политике конфиденциальности   —   Все права защищены 2021 С</div>
+      </div>
     </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,9 @@ export default function App ({ Component, pageProps }: any) {
     <>
       <Head>
         <title>WineDay</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Yeseva+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Yeseva+One&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
